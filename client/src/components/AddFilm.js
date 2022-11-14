d5600a6c12efe423b7ef05267b0d91605d191f03
Cyrille Asima <casima@terpmail.umd.edu>
2022-11-14 14:43:38 -0500

    more update on AddFilm comp

--- client/src/components/AddFilm.js
+++ client/src/components/AddFilm.js
@@ -14,7 +14,22 @@ const AddFilm = () => {
     const [factoids,setFactoids] = useState('')
     const [image,setImage] = useState('')
 
-
+const submitHandler = (e) => {
+    e.preventDefault()
+    axios.post ('http://localhost:8000/api/addFilm', {
+        title,
+        director,
+        genre,
+        yearReleased,
+        cast,
+        factoids,
+        image,
+    }).then((res)=>{
+        console.log(res)
+    }).catch((err)=>{
+        console.log(err)
+    })
+}
 
 
 return (
@@ -27,50 +42,51 @@ return (
         </div>
         <div>
             <h2>Add a Film:</h2>
-            <div class="main1">
-                <input type="text" value={title} onChange={(e)=>setTitle(e.target.value)} placeholder="Title"/>
-                <input type="text"  value={director} onChange={(e)=>setDirector(e.target.value)} placeholder="Director"/> 
-            </div>
-            <div class="main2">
-                <input type="number"  value={yearReleased} onChange={(e)=>setYearReleased(e.target.value)} placeholder="Year Released"/>
-                <select  value={genre} onChange={(e)=>setGenre(e.target.value)} >
-                    <option>Select A Genre</option>
-                    <option value="Comedy">Comedy</option>
-                    <option value="Drama">Drama</option>
-                    <option value="Horror">Horror</option>
-                    <option value="Sci-Fi">Sci-Fi</option>
-                    <option value="Action">Action</option>
-                    <option value="Family">Family</option>
-                    <option value="Documentary">Documentary</option>
-                    <option value="Romcomo">Romcomo</option>
-                    <option value="Silent Movie">Silent Movie</option>
-                    <option value="Thriller">Thriller</option>
-                    <option value="Crime Noir">Crime Noir</option>
-                    <option value="French Cinema">French Cinema</option>
-                    <option value="Horror/Comedy">Horror/Comedy</option>
-                    <option value="Kung-fu">Kung-fu</option>
-                    <option value="Bollywood">Bollywood</option>
-                    <option value="Anime">Anime</option>
-                </select>
-            </div>
+            <form onSubmit={submitHandler} >
+                <div class="main1">
+                    <input type="text" value={title} onChange={(e)=>setTitle(e.target.value)} placeholder="Title"/>
+                    <input type="text"  value={director} onChange={(e)=>setDirector(e.target.value)} placeholder="Director"/> 
+                </div>
+                <div class="main2">
+                    <input type="number"  value={yearReleased} onChange={(e)=>setYearReleased(e.target.value)} placeholder="Year Released"/>
+                    <select  value={genre} onChange={(e)=>setGenre(e.target.value)} >
+                        <option>Select A Genre</option>
+                        <option value="Comedy">Comedy</option>
+                        <option value="Drama">Drama</option>
+                        <option value="Horror">Horror</option>
+                        <option value="Sci-fi">Sci-Fi</option>
+                        <option value="Action">Action</option>
+                        <option value="Family">Family</option>
+                        <option value="Documentary">Documentary</option>
+                        <option value="Romcomo">Romcomo</option>
+                        <option value="Silent Movie">Silent Movie</option>
+                        <option value="Thriller">Thriller</option>
+                        <option value="Crime Noir">Crime Noir</option>
+                        <option value="French Cinema">French Cinema</option>
+                        <option value="Horror/Comedy">Horror/Comedy</option>
+                        <option value="Kung-fu">Kung-fu</option>
+                        <option value="Bollywood">Bollywood</option>
+                        <option value="Anime">Anime</option>
+                    </select>
+                </div>
 
-            <div class="main3">
-                <input type="text" value={cast} onChange={(e)=>setCast(e.target.value)} placeholder="Cast Members"/>
-            </div>
+                <div class="main3">
+                    <input type="text" value={cast} onChange={(e)=>setCast(e.target.value)} placeholder="Cast Members"/>
+                </div>
 
-            <div class="main4">
-                <input type="text" value={factoids} onChange={(e)=>setFactoids(e.target.value)} placeholder="Fun facts..."/>
-            </div>
+                <div class="main4">
+                    <input type="text" value={factoids} onChange={(e)=>setFactoids(e.target.value)} placeholder="Fun facts..."/>
+                </div>
 
-            <div class="button">
-            <button type='submit' className='btn btn-info mt-3'>Add Film</button>
+                <div class="image">
+                    <input type="text" value={image} onChange={(e)=>setImage(e.target.value)} placeholder="Add film box-art"/>
+                </div>
 
-            </div>
-
-
+                <div class="button">
+                    <button type='submit' className='btn btn-info mt-3'>Add Film</button>
+                </div>
+            </form>
         </div>
-
-        
     </div>
 
 )
